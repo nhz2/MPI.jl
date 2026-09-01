3e4b9106689b05f0cdf46f19add1d774b6afcda6
nhz2 <nhz2@cornell.edu>
2026-08-31 20:38:40 -0400

    Update oneAPI extenstion to match CUDA

--- ext/OneAPIExt.jl
+++ ext/OneAPIExt.jl
@@ -1,26 +1,17 @@
 module OneAPIExt
 
-import MPI
-isdefined(Base, :get_extension) ? (import oneAPI) : (import ..oneAPI)
-import MPI: MPIPtr, Buffer, Datatype
+using oneAPI: oneAPI
+using MPI: MPI, MPIPtr, Buffer, Datatype, CConvWrapper
 
-function Base.cconvert(::Type{MPIPtr}, A::oneAPI.oneArray{T}) where T
-    A
+function Base.cconvert(::Type{MPIPtr}, buf::oneAPI.oneArray{T}) where T
+    CConvWrapper(oneAPI.ZePtr{T}, buf)
 end
 
-function Base.unsafe_convert(::Type{MPIPtr}, X::oneAPI.oneArray{T}) where T
-    reinterpret(MPIPtr, Base.unsafe_convert(oneAPI.ZePtr{T}, X))
+function Base.cconvert(::Type{MPIPtr}, buf::SubArray{T,N,P,I,true}) where {T,N,P<:oneAPI.oneArray,I}
+    CConvWrapper(oneAPI.ZePtr{T}, buf)
 end
 
-# only need to define this for strided arrays: all others can be handled by generic machinery
-function Base.unsafe_convert(::Type{MPIPtr}, V::SubArray{T,N,P,I,true}) where {T,N,P<:oneAPI.oneArray,I}
-    X = parent(V)
-    pX = Base.unsafe_convert(oneAPI.ZePtr{T}, X)
-    pV = pX + ((V.offset1 + V.stride1) - first(LinearIndices(X)))*sizeof(T)
-    return reinterpret(MPIPtr, pV)
-end
-
-function Buffer(arr::oneAPI.oneArray)
+function MPI.Buffer(arr::oneAPI.oneArray)
     Buffer(arr, Cint(length(arr)), Datatype(eltype(arr)))
 end
 
